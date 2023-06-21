3424080d114280758411d9431d2926e925b8b94c
azafe <alvarozafe@gmail.com>
2023-06-21 14:48:10 -0300

    Add form

---
+++ src/components/todo.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+export default function Todo({ item }){
+
+    const [isEdit, setIsEdit] = useState(false);
+
+    function FormEdit(){
+        return <form>
+            <input type="text" />
+            <button>Update</button>
+        </form>
+    }
+
+    function TodoElement(){
+        return <div>       
+        {item.title}
+        <button onClick={() => setIsEdit(true)}>Edit</button>
+        <button>Delete</button>
+    </div>
+    }
+    return <div className="todo">
+        {isEdit ? <FormEdit/> : <TodoElement/>}
+        
+    </div>
+
+}
